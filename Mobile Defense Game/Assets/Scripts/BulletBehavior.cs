a19f9a3d34e48957d87e24d8fab0c9d4f54dc153
ndb796 <ndb796@naver.com>
2019-02-14 09:34:50 +0900

    총알 발사 관련 소스코드 개선

--- Mobile Defense Game/Assets/Scripts/BulletBehavior.cs
+++ Mobile Defense Game/Assets/Scripts/BulletBehavior.cs
@@ -3,23 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour {
+    
+    public BulletStat bulletStat { get; set; }
 
-    public float speed = 10.0f;
-    public GameObject character;
-
-    private int damage;
-
-    public void setDamage(int input)
+    public BulletBehavior()
     {
-        damage = input;
+        bulletStat = new BulletStat(0, 0);
     }
+
+    public GameObject character;
 
 	void Start () {
         Destroy(gameObject, 3.0f);
 	}
 	
 	void Update () {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        transform.Translate(Vector2.right * bulletStat.speed * Time.deltaTime);
 	}
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -27,7 +26,7 @@ public class BulletBehavior : MonoBehaviour {
         if(other.gameObject.tag == "Monster")
         {
             Destroy(gameObject);
-            other.GetComponent<MonsterStat>().attacked(damage);
+            other.GetComponent<MonsterStat>().attacked(bulletStat.damage);
         }   
     }
 }
