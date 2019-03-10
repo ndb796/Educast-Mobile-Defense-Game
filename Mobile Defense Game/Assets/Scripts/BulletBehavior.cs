3c3cb69dac867ec7785d72d0d949f5a535c32e3b
ndb796 <ndb796@naver.com>
2019-03-10 14:47:03 +0900

    코루틴 적용

--- Mobile Defense Game/Assets/Scripts/BulletBehavior.cs
+++ Mobile Defense Game/Assets/Scripts/BulletBehavior.cs
@@ -7,7 +7,6 @@ public class BulletBehavior : MonoBehaviour {
     public BulletStat bulletStat { get; set; }
 
     public float activeTime = 3.0f;
-    public float spawnTime;
 
     public BulletBehavior()
     {
@@ -19,22 +18,21 @@ public class BulletBehavior : MonoBehaviour {
     public void Spawn()
     {
         gameObject.SetActive(true);
-        spawnTime = Time.time;
     }
 
-	void Start () {
-        Spawn();
-	}
+    private void OnEnable()
+    {
+        StartCoroutine(BulletInactive(activeTime));
+    }
+
+    IEnumerator BulletInactive(float activeTime)
+    {
+        yield return new WaitForSeconds(activeTime);
+        gameObject.SetActive(false);
+    }
 	
 	void Update () {
-        if(Time.time - spawnTime >= activeTime)
-        {
-            gameObject.SetActive(false);
-        }
-        else
-        {
-            transform.Translate(Vector2.right * bulletStat.speed * Time.deltaTime);
-        }
+        transform.Translate(Vector2.right * bulletStat.speed * Time.deltaTime);
 	}
 
     private void OnTriggerEnter2D(Collider2D other)
